15013b3096c1bb169a9dcaa0f5cb57f28c60f037
Bilel Hawari <bilelhawari@gmail.com>
2017-07-16 06:06:40 +0100

    Update removing-resized-pictures.sh

--- wordpress/removing-resized-pictures.sh
+++ wordpress/removing-resized-pictures.sh
@@ -4,5 +4,5 @@ find . -regextype posix-extended -regex ".*-[[:digit:]]{1,9}x[[:digit:]]{1,9}(@2
 #This will delete all resized pictures (EX : file-100x100.jpg) in the current directory and it's subdirectories
 
 #We replace all pictures with a prefix (Ex: DSC_) by a keyword and rename all concerned 
-$ for filename in DSC_*.jpg; do mv $filename ${filename//DSC/landscape}; done
+for filename in DSC_*.jpg; do mv $filename ${filename//DSC/landscape}; done
 
