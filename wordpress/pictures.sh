d87a759e924a8bd006042914e167cebc9a75afeb
Bilel Hawari <bilelhawari@gmail.com>
2017-07-17 05:16:02 +0100

    Update and rename removing-resized-pictures.sh to pictures.sh

--- wordpress/pictures.sh
+++ wordpress/pictures.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-find . -regextype posix-extended -regex ".*-[[:digit:]]{1,9}x[[:digit:]]{1,9}(@2x)?.(jpg|jpeg|png|eps|gif)" -type f -exec rm {}  \;
-
 #This will delete all resized pictures (EX : file-100x100.jpg) in the current directory and it's subdirectories
+find . -regextype posix-extended -regex ".*-[[:digit:]]{1,9}x[[:digit:]]{1,9}(@2x)?.(jpg|jpeg|png|eps|gif)" -type f -exec rm {}  \;
 
 #We replace all pictures with a prefix (Ex: DSC_) by a keyword and rename all concerned 
 for filename in DSC_*.jpg; do mv $filename ${filename//DSC/landscape}; done
+
+#Reduce pictures size (Over 1Mb to half size)
+find -type f -name "*.jpg" -size +1M -exec mogrify -resize 50% {} \;
 
